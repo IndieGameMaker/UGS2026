09ed7864666f71978300dda02bcf954635805ccc
Lee JaeHyun <myleje@gmail.com>
2026-05-04 11:12:50 +0900

    Add score input field and save button functionality in ScoreManager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,3 +1,5 @@
+using System;
+using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
 using Unity.Services.Core;
@@ -8,4 +10,15 @@ public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField scoreIf;
     [SerializeField] private Button saveScoreButton;
+
+    private void OnEnable()
+    {
+        saveScoreButton.onClick.AddListener( () => AddScore());
+    }
+
+    private async void AddScore()
+    {
+        var response = await LeaderboardsService.Instance.AddPlayerScoreAsync("Ranking", int.Parse(scoreIf.text));
+        Debug.Log(JsonConvert.SerializeObject(response));
+    }
 }
