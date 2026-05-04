f3027d267c5b34f0ce2fb9b854a2777346bac082
Lee JaeHyun <myleje@gmail.com>
2026-05-04 11:05:08 +0900

    Add ScoreManager class with score input field and save button

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Leaderboards;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField scoreIf;
+    [SerializeField] private Bπutton saveScoreButton;
+}
