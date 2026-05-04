809401385ddee3ff84b478c07199a7c2ac525beb
Lee JaeHyun <myleje@gmail.com>
2026-05-04 10:11:40 +0900

    Add initial project files and configurations

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    [SerializeField] private Button loginButton;
+}
