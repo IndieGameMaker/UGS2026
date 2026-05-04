7ee65b28b3f67a3c23e8e313fd7901d570cbbed5
Lee JaeHyun <myleje@gmail.com>
2026-05-04 11:28:47 +0900

    Add delete user button in SampleScene and enhance score management with leaderboard functionality

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
 using Unity.Services.Core;
 using Unity.Services.Leaderboards;
+using Unity.Services.Leaderboards.Models;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
@@ -16,9 +18,24 @@ public class ScoreManager : MonoBehaviour
         saveScoreButton.onClick.AddListener( () => AddScore());
     }
 
+    // 랭킹 저장할 리스트
+    public List<LeaderboardEntry> entries = new();
+
     private async void AddScore()
     {
         var response = await LeaderboardsService.Instance.AddPlayerScoreAsync("Ranking", int.Parse(scoreIf.text));
         Debug.Log(JsonConvert.SerializeObject(response));
+
+        var ranking = await LeaderboardsService.Instance.GetScoresAsync("Ranking");
+        entries = ranking.Results;
+
+        string rank = "";
+        foreach (var entry in entries)
+        {
+            rank += $"[{entry.Rank}] {entry.PlayerName} : {entry.Score}\n";
+            // ScrollView의 Contents 하위에 프리팹 생성
+        }
+
+        Debug.Log(rank);
     }
 }
