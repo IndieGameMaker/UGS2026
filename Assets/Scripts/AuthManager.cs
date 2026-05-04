1e21ec51b2a410f6499c19c7573536f764c4221e
Lee JaeHyun <myleje@gmail.com>
2026-05-04 10:54:24 +0900

    Add player name input and save functionality in AuthManager

--- Assets/Scripts/AuthManager.cs
+++ Assets/Scripts/AuthManager.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using Unity.Services.Core;
 using Unity.Services.Authentication;
@@ -7,6 +9,8 @@ public class AuthManager : MonoBehaviour
 {
     [SerializeField] private Button loginButton;
     [SerializeField] private Button logoutButton;
+    [SerializeField] private Button savePlayerNameButton;
+    [SerializeField] private TMP_InputField playerNameIf;
 
     // async / await / Task
     private async void Start()
@@ -31,5 +35,19 @@ public class AuthManager : MonoBehaviour
         
         // 5. 로그아웃 처리
         logoutButton.onClick.AddListener(() => AuthenticationService.Instance.SignOut());
+        
+        // 6. 이름 변경
+        savePlayerNameButton.onClick.AddListener( async () => SetPlayerName(playerNameIf.text));
+    }
+
+    // 50자 허용
+    // 공백 허용 불가
+    // 이름#1234
+    private async Task SetPlayerName(string playerName)
+    {
+        await AuthenticationService.Instance.UpdatePlayerNameAsync(playerName);
+        
+        var savedPlayerName = await AuthenticationService.Instance.GetPlayerNameAsync();
+        Debug.Log("이름 저장완료: " + savedPlayerName);
     }
 }
